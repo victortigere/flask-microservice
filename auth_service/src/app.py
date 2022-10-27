e541ca02bf8f6664f007a9e25f76827ce025b0bf
victortigere <tigerevice@gmail.com>
2022-10-27 05:24:27 +0200

    auth, match service containerized separately

--- auth_service/src/app.py
+++ auth_service/src/app.py
@@ -4,14 +4,14 @@ import socket
 app = Flask(__name__)
 
 
-@app.route("/health")
+@app.route("/auth/health")
 def get_api_status():
     return jsonify({
                    "code": 00,
-                   "description": "API status"})
+                   "description": "API statuss"})
 
 
-@app.route("/details")
+@app.route("/auth/details")
 def get_details():
     host_name = socket.gethostname()
     host_ip = socket.gethostbyname(host_name)
